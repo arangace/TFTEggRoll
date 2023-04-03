97358ca17aa6c8b61d4d593b709f68a6c880fad1
arangace <ahua291@aucklanduni.ac.nz>
2023-04-03 16:48:10 +1200

    added images to reflect results

---
+++ components/EggImage.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+type EggImageProps = {
+  eggImage: string;
+};
+const EggImage = ({ eggImage }: EggImageProps) => {
+  const [imageSrc, setImageSrc] = useState("");
+  useEffect(() => {
+    setImageSrc(`/images/${eggImage}`);
+  }, [eggImage]);
+
+  return (
+    <div>
+      <Image alt="tft-egg-image" src={imageSrc} width={200} height={200} />
+    </div>
+  );
+};
+
+export default EggImage;
--- components/Gacha.tsx
+++ components/Gacha.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "../styles/gacha.module.scss";
 import EggRoll from "./EggRoll";
-
+import EggImage from "./EggImage";
 type rollResult = any;
 const Gacha = () => {
   const handleOnClick = () => {
@@ -9,20 +9,25 @@ const Gacha = () => {
   };
   const [rollResult, setrollResult] = useState<rollResult>();
   const [bestPrizeWon, setbestPrizeWon] = useState(false);
+  const [eggImage, seteggImage] = useState("zed-cache.png");
 
   const roll = () => {
     if (bestPrizeWon) {
       setbestPrizeWon(false);
+      seteggImage("zed-cache.png");
     }
     const grandPrize = 0.2;
     const roll = Math.random();
     setrollResult(roll.toFixed(2));
     if (roll <= grandPrize) {
       setbestPrizeWon(true);
+      seteggImage("project-zed.webp");
     }
   };
   return (
     <div className={styles.container}>
+      <EggImage eggImage={eggImage} />
+
       <div>roll: {rollResult}</div>
       <div>{bestPrizeWon ? "You Won" : "Keep trying!"}</div>
       <button onClick={handleOnClick} className={styles.rollButton}>
