d4ef997d02975815d9ec7caebc7c5b6337c5566d
arangace <ahua291@aucklanduni.ac.nz>
2023-04-03 16:30:55 +1200

    added base probability rolling functionality

--- components/Gacha.tsx
+++ components/Gacha.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/gacha.module.scss";
+import EggRoll from "./EggRoll";
 
+type rollResult = any;
 const Gacha = () => {
+  const handleOnClick = () => {
+    roll();
+  };
+  const [rollResult, setrollResult] = useState<rollResult>();
+  const [bestPrizeWon, setbestPrizeWon] = useState(false);
+
+  const roll = () => {
+    if (bestPrizeWon) {
+      setbestPrizeWon(false);
+    }
+    const grandPrize = 0.2;
+    const roll = Math.random();
+    setrollResult(roll.toFixed(2));
+    if (roll <= grandPrize) {
+      setbestPrizeWon(true);
+    }
+  };
   return (
     <div className={styles.container}>
-      Gacha
-      <button className={styles.rollButton}>Open</button>
+      <div>roll: {rollResult}</div>
+      <div>{bestPrizeWon ? "You Won" : "Keep trying!"}</div>
+      <button onClick={handleOnClick} className={styles.rollButton}>
+        Open
+      </button>
     </div>
   );
 };
