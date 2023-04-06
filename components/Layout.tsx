4bb412e29ede507b9a9c788d4d7bfda1f6154181
arangace <ahua291@aucklanduni.ac.nz>
2023-04-06 23:29:10 +1200

    cleaner UI with github link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,9 +7,15 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div>
+    <div className="main-container">
       <NavBar />
       <main className="main-content">{children}</main>
+      <footer className="footer">
+        <span className="footer-author">Developed by: Andy Huang</span>
+        <span className="footer-link">
+          <a href="https://github.com/arangace/TFTEggRoll">GitHub Link </a>
+        </span>
+      </footer>
     </div>
   );
 }
