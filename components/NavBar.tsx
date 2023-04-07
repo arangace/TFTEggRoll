17b74d68c94cd6833f379b509a07f45f409f5424
arangace <ahua291@aucklanduni.ac.nz>
2023-04-07 18:03:24 +1200

    removed functionalityy of logo to just be a picture

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,27 +8,26 @@ const NavBar = () => {
   return (
     <nav className={styles.container}>
       <div className={styles.logo}>
-        <Link className={styles.logoLink} href="/">
-          <Image
-            width="100"
-            height="100"
-            alt="TFT Egg Roll Logo"
-            src="/images/logo-light.png"
-          />
-        </Link>
+        {/* <Link className={styles.logoLink} href="/"> */}
+        <Image
+          width="100"
+          height="100"
+          alt="TFT Egg Roll Logo"
+          src="/images/logo-light.png"
+        />
+        {/* </Link> */}
       </div>
       <ul className={styles.navContainer}>
-        {/* TODO later functionality */}
+        {/* TODO Future functionality */}
         {/* <li>
           <Link href="">Login</Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           <button
+            aria-label="Dark Mode Button"
             onClick={handleDarkModeButtonClick}
             className={styles.darkModeButton}
-          >
-            DarkMode
-          </button>
+          ></button>
         </li> */}
       </ul>
     </nav>
